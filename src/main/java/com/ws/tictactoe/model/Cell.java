33c76610d6827bcfe8ea4cb0e86fa71a11d10614
Saket Puranik <puranik.saket@gmail.com>
2017-10-28 21:03:28 +0530

    @Saket Refactored a Cell

--- src/main/java/com/ws/tictactoe/model/Cell.java
+++ src/main/java/com/ws/tictactoe/model/Cell.java
@@ -2,8 +2,16 @@ package com.ws.tictactoe.model;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
 
-public class Cell {public final int row;
+@EqualsAndHashCode
+public class Cell {
+
+    @Getter
+    public final int row;
+
+    @Getter
     public final int column;
 
     @JsonCreator
@@ -13,50 +21,4 @@ public class Cell {public final int row;
     }
 
 
-    public int getColumn() {
-        return this.column;
-    }
-
-    public int getRow() {
-        return this.row;
-    }
-
-
-    public static int getDistance(Cell pos1, Cell pos2) {
-        int hDist = Math.abs(pos1.getRow() - pos2.getRow());
-        int vDist = Math.abs(pos1.getColumn() - pos2.getColumn());
-
-        if (hDist == vDist) { // diagonally aligned
-            return hDist;
-        } else {
-            return hDist + vDist;
-        }
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Cell)) return false;
-
-        Cell cell = (Cell) o;
-
-        if (row != cell.row) return false;
-        return column == cell.column;
-
-    }
-
-    @Override
-    public int hashCode() {
-        int result = row;
-        result = 31 * result + column;
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "Cell{" +
-                "row=" + row +
-                ", column=" + column +
-                '}';
-    }
 }
